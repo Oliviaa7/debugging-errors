d83f283b5fdaca42c71a80d0335961a2672e89f8
Oliviaa7 <olivialanderson1994@gmail.com>
2024-09-28 22:37:05 -0700

    fixed errors on average.js and reverse.js

--- reverse.js
+++ reverse.js
@@ -1,8 +1,10 @@
-var input = process.argv[2];
+const input = process.argv[2];
 
-if (input)
+const reverse = function(original) {
+  return original.split('').reverse().join('');
+};
+
+if (input) {
   console.log(reverse(input));
-
-function reverse(original) {
-  return orignal.split('').reverseList().join('');
 }
+
